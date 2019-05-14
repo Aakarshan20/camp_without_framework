9579ca13494616274f12bfc950f17d5fd4caac74
aabbcc <iiooiio@hotmail.com>
2019-05-14 15:36:56 +0800

    update

---
+++ include/config.ini.php
@@ -0,0 +1,11 @@
+<?php
+defined('ACC')||exit('ACC Denied');
+
+$_CFG= array();
+$_CFG['host'] = 'localhost';
+$_CFG['db'] = 'camp';
+$_CFG['user'] = 'hisql0726';
+$_CFG['passwd'] = 'hisql0726sqlhi';
+$_CFG['char'] = 'utf8';//
+
+?>
